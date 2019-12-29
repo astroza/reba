25e0bd27b97918e113018b7ad49e4720b0dab29f
Felipe Astroza <felipe@astroza.cl>
2019-12-28 22:12:12 -0300

    v8_build.sh dropped gm.y in favour of the manual workflow to add gn args prior to ninja run v8_objs were sorted manually to remove -Wl,start-group v8 repo revision fixed

--- tools/build_v8.sh
+++ tools/build_v8.sh
@@ -12,14 +12,5 @@ export PATH=$PATH:$(pwd)/depot_tools
 cd ../deps
 gclient sync
 cd v8
-tools/dev/gm.py $ARCH.release
-cat << __EOF__ > out/$ARCH.release/args.gn
-is_debug = false
-target_cpu = "$ARCH"
-v8_static_library = true
-is_component_build = false
-is_clang = true
-use_custom_libcxx_for_host=false
-use_custom_libcxx=false
-__EOF__
-autoninja -C out/$ARCH.release d8
+gn gen out/$ARCH.release --args='is_debug=false target_cpu="'$ARCH'" v8_static_library=true is_component_build=false is_clang=true use_custom_libcxx_for_host=false use_custom_libcxx=false'
+ninja -C out/$ARCH.release d8
