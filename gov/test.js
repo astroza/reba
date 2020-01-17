7ff92b0d1760832ae1f633d66ff60cc701081b28
Felipe Astroza <felipe@astroza.cl>
2020-01-17 01:31:53 -0300

    gov example updated

--- gov/test.js
+++ gov/test.js
@@ -1,1 +1,6 @@
-var worker_group = new WorkerGroup('');
+console.log("* Fiberware Lake governator loaded");
+var groups = [];
+
+var worker_script = "addEventListener('fetch', () => console.log('Test'));";
+var group = new WorkerGroup(worker_script);
+router.addHost('localhost:8080', group);
