85a21f258424503ea6bd8c53776bc433227177ca
Felipe Astroza <felipe@astroza.cl>
2020-07-28 02:23:24 -0400

    main formatting

--- src/main.cc
+++ src/main.cc
@@ -1,33 +1,30 @@
 
-#include <iostream>
-#include <ctime>
 #include <boost/asio.hpp>
-#include <boost/beast/core.hpp>
 #include <boost/asio/ip/address.hpp>
+#include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
+#include <ctime>
 #include <http.h>
-#include <worker_group.h>
+#include <iostream>
 #include <rebaapi.h>
+#include <worker_group.h>
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
-	{
-		std::cerr << "Usage: reba <governator script file> <port>\n";
-		return 1;
-	}
-	reba::engine::init();
+    if (argc != 3) {
+        std::cerr << "Usage: reba <governator script file> <port>\n";
+        return 1;
+    }
+    reba::engine::init();
     std::srand(std::time(nullptr));
-	std::ifstream gov_script_file { argv[1] };
-	std::string gov_script { std::istreambuf_iterator<char>(gov_script_file), std::istreambuf_iterator<char>() };
+    std::ifstream gov_script_file { argv[1] };
+    std::string gov_script { std::istreambuf_iterator<char>(gov_script_file), std::istreambuf_iterator<char>() };
     auto http_server = reba::http::Server(&rebaapi::router::g_default_router);
     // Governator is a special WorkerGroup that controls Lake instance
-	auto *gov = new reba::WorkerGroup(gov_script, true);
+    auto* gov = new reba::WorkerGroup(gov_script, true);
     try {
         http_server.Start(boost::asio::ip::make_address("0.0.0.0"), static_cast<unsigned short>(std::atoi(argv[2])));
-    }
-    catch (const std::exception& e)
-    {
+    } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
